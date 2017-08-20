a7ed7de4a678b1b6753d3ef10c2d58d84c5cac03
Kai Aragaki <7370516+KaiAragaki@users.noreply.github.com>
2017-08-20 10:25:55 -0700

    It works now.
    Completely rudimentary. No error bars. No % over glucose. No way to even save these charts. Ambiguous labeling without customization support. But it works. "Works".

--- insulinReader.R
+++ insulinReader.R
@@ -1,7 +1,10 @@
 insulinReader <- function(dateArg){
-  # Assumptions: Assumes duplicate of duplicate (ie SOP, 4 well per cond)
+  require(tidyverse)
+  # Assumptions
+  # Assumes duplicate of duplicate (ie SOP, 4 well per cond)
+  # Assumes condition layout is similar or identical
   # Reads the file, skipping the (uneeded) header, and packs it in a tibble
-  path <- file.path("~", "Desktop", "Lab", "ELISA Text Files", "Adam") %>%
+  path <- file.path("C:", "Users", "Adam", "Desktop") %>%
     setwd()
   insFileName <- paste0(dateArg, "_rat.txt")
   insFile <- readLines(insFileName, encoding = "latin1")
@@ -25,8 +28,20 @@ insulinReader <- function(dateArg){
   conditionNames <- paste("Condition", 1:(nrow(conditionData)/4)) %>%
     rep(each = 4)
   conditionData[,10] <- conditionNames
-    
-  View(conditionData)
+  
+  groupCond <- as.tibble(conditionData) %>%
+    group_by(V10) %>%
+    mutate("numericMeanConc" = as.numeric(as.character(Mean_Conc))) %>%
+    mutate("conditionMean" = mean(numericMeanConc)) %>%
+    mutate("adjCondMean" = mean(conditionMean/4)) %>%
+    mutate("condSD" = sd(c(numericMeanConc[1]/4, numericMeanConc[3]/4))) #Choosing all values would result in overcounting and lower SD - not cool.
+  
+  View(groupCond)
+  
+  # I'll still need the SEM, % above glucose, and labelling capabilities, but for now I'll try plotting this mess.
+  
+  ggplot(groupCond, mapping = aes(x = V10, y = adjCondMean, fill = V10)) +
+    geom_col()
   
   # After all that drama, standardData and conditionData now are (semi) normal tables that can be worked with
 
